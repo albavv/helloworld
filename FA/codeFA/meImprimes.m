7c5a924cddbef52503b663925fb46a79566ba819
albavv <aaroky1@gmail.com>
2018-09-06 11:32:40 +0200

    Revert "Delete Multiclase"
    This reverts commit b67c26f6263d11f99efde743dd18d24af0bee32f.

---
+++ FA/codeFA/meImprimes.m
@@ -0,0 +1,8 @@
+function meImprimes(nombre)
+ 
+ 
+h = gcf;
+set(h,'Units','Inches');
+pos = get(h,'Position');
+set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)]);
+print(h,nombre,'-dpdf','-r0');
