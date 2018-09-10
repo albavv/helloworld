d193557135d91389561979d52157a5c3a733117d
albavv <aaroky1@gmail.com>
2018-09-10 12:41:47 +0200

    delete

--- FA/codeFA/meImprimes.m
+++
@@ -1,8 +0,0 @@
-function meImprimes(nombre)
- 
- 
-h = gcf;
-set(h,'Units','Inches');
-pos = get(h,'Position');
-set(h,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)]);
-print(h,nombre,'-dpdf','-r0');
